cdd3a19429f5deeb60fcd9ae48a673c9f6bd5fed
Oscar Wahlberg <oscwa331@gmail.com>
2016-03-23 11:18:43 +0100

    Onsdag. Oscar försöker dela app-projektet med gruppen

---
+++ app/src/main/java/com/example/oscar/nackademiskaauktionsframjandet/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.oscar.nackademiskaauktionsframjandet;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Blob;
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ArrayList<Auction> auctionList = new ArrayList<Auction>();
+
+        // OBS!! Lägg till volley (compile 'com.android.volley:volley:1.0.0') i Gradle (build.gradle (module:app))
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, "http://nackademiska.azurewebsites.net/2/getongoingauctions", null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject j = response.getJSONObject(i);
+
+                                Auction a = new Auction();
+                                a.Id = j.getInt("Id");
+                                a.Name = j.getString("Name");
+                                a.Description = j.getString("Description");
+                                a.StartTime = j.getString("StartTime");
+                                a.EndTime = j.getString("EndTime");
+                                a.Image = j.getString("Image");
+                                a.CategoryId = j.getInt("CategoryId");
+                                a.SupplierId = j.getInt("SupplierId");
+                                a.AcceptPrice = j.getInt("AcceptPrice");
+                                a.Sold = j.getBoolean("Sold");
+
+                                auctionList.add(a);
+                            }
+
+                            ArrayAdapter<Auction> arrayAdapter = new ArrayAdapter<Auction>(MainActivity.this,
+                                    android.R.layout.simple_list_item_1, android.R.id.text1, auctionList);
+
+                            ListView listView = (ListView) findViewById(R.id.listView);
+
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent i = new Intent(MainActivity.this, AuctionListActivity.class);
+                                    i.putExtra("auctionId", auctionList.get(position).Id);
+                                    startActivity(i);
+                                }
+                            });
+
+                            listView.setAdapter(arrayAdapter);
+
+
+
+                        } catch (JSONException e) {
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+        queue.add(jsArrayRequest);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.oscar.nackademiskaauktionsframjandet/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.oscar.nackademiskaauktionsframjandet/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
